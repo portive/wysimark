3be72a34b57341d89c5d66de1e849180a7e33520
Sunny Hirai <thesunny@gmail.com>
2022-03-28 17:52:09 -0700

    Add TODO for rollup-plugin-analyze-deps unit test

--- packages/editor/lib/rollup-plugin-analyze-deps/test/rollup-analyze-deps.test.ts
+++ packages/editor/lib/rollup-plugin-analyze-deps/test/rollup-analyze-deps.test.ts
@@ -1,6 +1,6 @@
-import { analyzeDeps } from "../analyze-deps"
-import analysis from "./analysis.json"
-import pkg from "./pkg.json"
+// import { analyzeDeps } from "../analyze-deps"
+// import analysis from "./analysis.json"
+// import pkg from "./pkg.json"
 
 // function clean(s: string) {
 //   return s.replace(/\0/g, "")
@@ -10,13 +10,19 @@ import pkg from "./pkg.json"
 //   return lines.map(clean)
 // }
 
-describe("rollup-analyze-deps", () => {
+export {}
+
+/**
+ * TODO: Add unit tests to rollup-plugin-analyze-deps
+ */
+
+describe("rollup-plugin-analyze-deps", () => {
   it("should analyze deps", async () => {
     // const s = "\u0000commonjsHelpers.js"
     // const s2 = "commonjsHelpers.js"
     // console.log(s === s.trim())
     // console.log("match?", clean(s).match(/^commonjs/))
     // console.log("match?", s2.trim().match(/^commonjs/))
-    analyzeDeps(analysis, pkg.dependencies)
+    // analyzeDeps(analysis, pkg.dependencies)
   })
 })
