b4f65ea33f5edc79540cd8161230443aa6161c4e
Sunny Hirai <thesunny@gmail.com>
2022-12-09 15:19:25 -0800

    Refactor navigation

--- src/table-plugin/methods/navigation.ts
+++ src/table-plugin/methods/navigation.ts
@@ -4,14 +4,27 @@ import { selectStartOfElement } from "~/src/sink"
 
 import { getTableInfo } from "./get-table-info"
 
+/**
+ * arrow down
+ */
 export function down(editor: Editor): boolean {
   const t = getTableInfo(editor)
+  /**
+   * exit if we're not in a table
+   */
   if (!t) return false
   const { cellIndex, rowIndex, rowCount, tablePath } = t
+  /**
+   * if we aren't in the last row of a table, move down a row
+   */
   if (rowIndex < rowCount - 1) {
     selectStartOfElement(editor, [...tablePath, rowIndex + 1, cellIndex])
     return true
   }
+  /**
+   * If we are in the last row of a table, move to the start of the Element
+   * after the table
+   */
   try {
     selectStartOfElement(editor, Path.next(tablePath))
     return true
@@ -20,14 +33,27 @@ export function down(editor: Editor): boolean {
   }
 }
 
+/**
+ * arrow up
+ */
 export function up(editor: Editor): boolean {
   const t = getTableInfo(editor)
+  /**
+   * exit if we're not in a table
+   */
   if (!t) return false
   const { cellIndex, rowIndex, tablePath } = t
+  /**
+   * If we aren't in the first row of a table, move up a row
+   */
   if (rowIndex > 0) {
     selectStartOfElement(editor, [...tablePath, rowIndex - 1, cellIndex])
     return true
   }
+  /**
+   * If we are in the first row of a table, move to the start of the Element
+   * before the table.
+   */
   try {
     selectStartOfElement(editor, Path.previous(tablePath))
     return true
