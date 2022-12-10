5818de44653e2421d22073cd2adb82739f5a49af
Sunny Hirai <thesunny@gmail.com>
2022-12-09 23:48:02 -0800

    Fix typing

--- src/table-plugin/methods/insert-column.ts
+++ src/table-plugin/methods/insert-column.ts
@@ -14,10 +14,18 @@ export function insertColumnOffset(
   const { tableElement, tablePath, cellIndex } = t
   const nextCellIndex = cellIndex + offset
   Editor.withoutNormalizing(editor, () => {
-    const columns = tableElement.columns
+    const { columns } = tableElement
     const nextColumns = [...columns]
+    /**
+     * Insert a Column into `TableElement.columns` which is the same as the
+     * value of the current column. This is the `alignment` of the column.
+     */
     nextColumns.splice(nextCellIndex, 0, columns[nextCellIndex])
     Transforms.setNodes(editor, { columns: nextColumns }, { at: tablePath })
+
+    /**
+     * Insert a `TableCell` at the correct spot.
+     */
     tableElement.children.forEach((rowElement, i) => {
       Transforms.insertNodes(editor, createCell(nextCellIndex), {
         at: [...tablePath, i, nextCellIndex],
@@ -27,10 +35,16 @@ export function insertColumnOffset(
   return true
 }
 
-export function insertColumnLeft(editor: Editor, { at }: { at?: MatchAt }) {
+export function insertColumnLeft(
+  editor: Editor,
+  { at }: { at?: MatchAt } = {}
+) {
   return insertColumnOffset(editor, { at })
 }
 
-export function insertColumnRight(editor: Editor, { at }: { at?: MatchAt }) {
+export function insertColumnRight(
+  editor: Editor,
+  { at }: { at?: MatchAt } = {}
+) {
   return insertColumnOffset(editor, { at, offset: 1 })
 }
