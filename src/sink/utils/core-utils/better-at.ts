4a7dd7cfa5b459227c847d9605037962a87b555c
Sunny Hirai <thesunny@gmail.com>
2022-12-16 00:17:25 -0800

    Add some docs for BetterAt

--- src/sink/utils/core-utils/better-at.ts
+++ src/sink/utils/core-utils/better-at.ts
@@ -2,12 +2,17 @@ import { Editor, Element, Location } from "slate"
 import { ReactEditor } from "slate-react"
 
 /**
- * We are able to match a `Location`, an `Element` or a `Selection` which
- * could return `null`
+ * Defines a value you'd find in a function's parameters as a replacement for
+ * `at`. The benefit of using `BetterAt` is that it allows you to search
+ * using an `Element`.
  */
-
 export type BetterAt = Location | Element | null
-export function betterAt(editor: Editor, at: Location | Element) {
+
+/**
+ * Takes a `BetterAt` type which can include an `Element` and returns a
+ * Location.
+ */
+export function betterAt(editor: Editor, at: Location | Element): Location {
   if (!Element.isElement(at)) return at
   return ReactEditor.findPath(editor, at)
 }
