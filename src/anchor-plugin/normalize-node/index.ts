7ec1bf538ce93794b5e1b7676139c2ff4f88b4c4
Sunny Hirai <thesunny@gmail.com>
2022-12-19 13:16:28 -0800

    Improve comments

--- src/anchor-plugin/normalize-node/index.ts
+++ src/anchor-plugin/normalize-node/index.ts
@@ -1,5 +1,26 @@
 import { Editor, Element, Node, NodeEntry, Transforms } from "slate"
 
+/**
+ * If there is an anchor node inside an anchor node, unwrap the inner anchor
+ * node as that is not allowed.
+ *
+ * Another approach would be to lift the nodes but I think unwrapping the
+ * inner one makes more sense. Consider these two scenarios (keeping in mind
+ * that they will likely be rare):
+ *
+ * - User selects text that includes a link. Then user links it. The user would
+ *   have a strong expectation that the link just applied would override the
+ *   link on the inside.
+ *
+ * - The opposite: User select text inside an existing link then tries to link
+ *   it. That's unusual and while the user may expect that inner selection to
+ *   get linked, I think this is a rare enough case whereas the prior one seems
+ *   like a slightly more natural one.
+ *
+ * We could, of course, solve both of these by adding more code paths at the
+ * time the link is inserted. That won't change this normalization code and
+ * not sure if it is worth the extra complexity.
+ */
 export function normalizeNode(editor: Editor, entry: NodeEntry<Node>): boolean {
   if (!Element.isElement(entry[0])) return false
   if (entry[0].type !== "anchor") return false
--- src/sink/utils/core-utils/curry.ts
+++ src/sink/utils/core-utils/curry.ts
@@ -1,6 +1,6 @@
 /**
- * Takes a function and some value which satisfies just the first argument in
- * the function.
+ * Takes (argument 1) a function and pre-fills the first argument of that
+ * function with (argument 2) the value to pre-fill the function with.
  *
  * Returns a new function with the first argument of the function pre-filled
  * using the 2nd arguments.
@@ -8,7 +8,7 @@
  * NOTE:
  *
  * This is not a full implementation of a curry but gives us what we want in
- * a lightweight manner with low complexity
+ * a lightweight manner, with low complexity and good typing.
  */
 export function curry<CA, RA extends unknown[], R>(
   fn: (curriedArg: CA, ...restArgs: RA) => R,
