3864517d75cbad6367dec6d98fa2829d9304a7e2
Sunny Hirai <thesunny@gmail.com>
2022-12-26 16:00:32 -0800

    Refactor normalize-sibling-paragraphs

--- src/collapsible-paragraph-plugin/normalize-node/index.ts
+++ src/collapsible-paragraph-plugin/normalize-node/index.ts
@@ -1,8 +1,6 @@
-import { Editor, Element, Node, NodeEntry, Transforms } from "slate"
+import { Editor, Element, Node, NodeEntry } from "slate"
 
-import { normalizeSiblings } from "~/src/sink"
-
-import { ParagraphElement } from ".."
+import { normalizeSiblingParagraphs } from "./normalize-sibling-paragraphs"
 import { normalizeSiblingWalls } from "./normalize-sibling-walls"
 
 export function normalizeNode(editor: Editor, entry: NodeEntry<Node>): boolean {
@@ -11,33 +9,4 @@ export function normalizeNode(editor: Editor, entry: NodeEntry<Node>): boolean {
   if (normalizeSiblingWalls(editor, [node, path])) return true
   if (normalizeSiblingParagraphs(editor, [node, path])) return true
   return false
-}
-
-/**
- * TODO:
- * FIXME:
- *
- * In order to make this normalization work, we must first enable a plugin
- * that we haven't created that I'm going to call `better-delete-plugin` which
- * will do two things
- *
- * - Execute a normalzation pass when a backspace or delete happens.
- */
-
-function isParagraph(node: Element): node is ParagraphElement {
-  return Element.isElement(node) && node.type === "paragraph"
-}
-
-function normalizeSiblingParagraphs(
-  editor: Editor,
-  entry: NodeEntry<Element>
-): boolean {
-  return normalizeSiblings(editor, entry, (a, b) => {
-    if (!isParagraph(a[0]) || !isParagraph(b[0])) return false
-    if (a[0].__collapsible && b[0].__collapsible) {
-      Transforms.removeNodes(editor, { at: a[1] })
-      return true
-    }
-    return false
-  })
 }
