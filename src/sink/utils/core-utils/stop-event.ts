1f247f9844181327f2c4a48418acb6fb29e587b4
Sunny Hirai <thesunny@gmail.com>
2023-01-05 09:19:22 -0800

    Add comments to stopEvent

--- src/sink/utils/core-utils/stop-event.ts
+++ src/sink/utils/core-utils/stop-event.ts
@@ -1,3 +1,7 @@
+/**
+ * Tiny helper to call `e.preventDefault()` and `e.stopPropagation()` as the
+ * same time.
+ */
 export function stopEvent(e: Event | React.SyntheticEvent) {
   e.preventDefault()
   e.stopPropagation()
