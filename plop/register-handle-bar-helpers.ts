51fe62297288cd14e7570ee80789397aafdcae6f
Sunny Hirai <thesunny@gmail.com>
2023-01-09 18:06:18 -0800

    Fix peer deps except the @sunny/assert-type one

--- plop/register-handle-bar-helpers.ts
+++ plop/register-handle-bar-helpers.ts
@@ -1,23 +1,24 @@
-import helpersConfig from "handlebars-helpers"
-import { NodePlopAPI } from "plop"
+export {}
+// import helpersConfig from "handlebars-helpers"
+// import { NodePlopAPI } from "plop"
 
-/**
- * Register handlebar helpers with plop
- *
- * https://github.com/plopjs/plop/issues/281
- */
-const helpers = helpersConfig()
-export function registerHandleBarHelpers(plop: NodePlopAPI): void {
-  for (const prop in helpers) {
-    // if it is not an already included "case" helper, than add the helper to plop
-    if (!prop.toLowerCase().includes("case")) {
-      plop.setHelper(prop, helpers[prop])
-    }
-  }
+// /**
+//  * Register handlebar helpers with plop
+//  *
+//  * https://github.com/plopjs/plop/issues/281
+//  */
+// const helpers = helpersConfig()
+// export function registerHandleBarHelpers(plop: NodePlopAPI): void {
+//   for (const prop in helpers) {
+//     // if it is not an already included "case" helper, than add the helper to plop
+//     if (!prop.toLowerCase().includes("case")) {
+//       plop.setHelper(prop, helpers[prop])
+//     }
+//   }
 
-  // overwrite "raw" helper afterwards, because it's not able to
-  // avoid escaping of {{{{raw}}}} block content otherwise
-  plop.setHelper("raw", (options: Handlebars.HelperOptions) => {
-    return options.fn(undefined)
-  })
-}
+//   // overwrite "raw" helper afterwards, because it's not able to
+//   // avoid escaping of {{{{raw}}}} block content otherwise
+//   plop.setHelper("raw", (options: Handlebars.HelperOptions) => {
+//     return options.fn(undefined)
+//   })
+// }
