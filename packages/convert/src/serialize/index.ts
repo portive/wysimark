7fcf575098c04835486fadfa013fb8bc5858d23f
Sunny Hirai <thesunny@gmail.com>
2023-02-25 23:01:55 -0800

    Fix indent issues in list serialization

--- packages/convert/src/serialize/index.ts
+++ packages/convert/src/serialize/index.ts
@@ -1,7 +1,9 @@
 import { Element } from "wysimark/src"
 
+import { normalizeElementListDepths } from "./normalize/normalizeElementListDepths"
 import { serializeElements } from "./serialize-elements"
 
 export function serialize(elements: Element[]): string {
-  return serializeElements(elements)
+  const normalizedElements = normalizeElementListDepths(elements)
+  return serializeElements(normalizedElements)
 }
--- packages/convert/src/test/list/list-unusual-children.test.ts
+++ packages/convert/src/test/list/list-unusual-children.test.ts
@@ -1,26 +1,15 @@
 import { check } from "../test-utils"
 
 describe("list unsupported", () => {
-  /**
-   * TODO:
-   *
-   * When a list item has children that are not list items like a heading,
-   * the list item should be closed and the heading should be parsed as a
-   * sibling of the list.
-   *
-   * This works; however, the following unordered-list-item is at a deeper
-   * level in this example. And when it's serialized, it comes out with some
-   * leading spaces. When the first item starts with leading spaces, even
-   * though it looks like a list, the Markdown interpreter will interpret
-   * this as a code-block.
-   */
   it("should parse a list with children that are not list items", async () => {
     check(
       `- alpha
 
 - # heading
 
-    - alpha`,
+    - alpha
+    
+        - alpha`,
       [
         {
           type: "unordered-list-item",
@@ -33,12 +22,37 @@ describe("list unsupported", () => {
           depth: 1,
           children: [{ text: "alpha" }],
         },
+        {
+          type: "unordered-list-item",
+          depth: 2,
+          children: [{ text: "alpha" }],
+        },
       ],
       `- alpha
 
 # heading
 
-    - alpha`
+- alpha
+
+    - alpha`,
+      [
+        {
+          type: "unordered-list-item",
+          depth: 0,
+          children: [{ text: "alpha" }],
+        },
+        { type: "heading", level: 1, children: [{ text: "heading" }] },
+        {
+          type: "unordered-list-item",
+          depth: 0,
+          children: [{ text: "alpha" }],
+        },
+        {
+          type: "unordered-list-item",
+          depth: 1,
+          children: [{ text: "alpha" }],
+        },
+      ]
     )
   })
 })
