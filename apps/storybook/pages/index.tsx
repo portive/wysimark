6040031c28b7b39364f379fbf36301eb678b831a
Sunny Hirai <thesunny@gmail.com>
2023-07-25 18:33:57 -0700

    Remove storybook and fix some warnings

--- apps/storybook/pages/index.tsx
+++
@@ -1,8 +0,0 @@
-export default function Index() {
-  return (
-    <div>
-      <h1>Hello World</h1>
-      <p>Lorem ipsum dolar sit amet consecteteur.</p>
-    </div>
-  )
-}
--- apps/vue-demo/.eslintrc.js
+++ apps/vue-demo/.eslintrc.js
@@ -1,4 +1,18 @@
+const customConfig = require("eslint-config-custom")
+
+/**
+ * The changes here are to remove React from eslintrc to get rid of the React
+ * warnings for this `vue-demo` app.
+ */
+
+// Remove the 'settings.react' key
+delete customConfig.settings.react
+
 module.exports = {
   root: true,
-  extends: ["custom"],
+  ...customConfig,
+  // Remove 'plugin:react/recommended' from the extends array
+  extends: customConfig.extends.filter((s) => s !== "plugin:react/recommended"),
+  // Remove 'react' plugin from the plugins array
+  plugins: customConfig.plugins.filter((plugin) => plugin !== "react"),
 }
